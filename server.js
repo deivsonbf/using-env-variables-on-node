87abe5de66ff105612fe3cf9809173722f1b1dd0
Deivsonbf <deivsonb7@gmail.com>
2021-11-19 21:28:49 -0300

    teste

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./index');
 const port = process.env.PORT || 5000
 
 app.listen(port, (req, res) => {
-    console.log(`Estou no ambiente de : ${process.env.NODE_ENV}`);
+    console.log("Estou no ambiente de : " + process.env.NODE_ENV);
 })
