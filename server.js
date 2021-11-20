6686b384009a672823ba5bea3f4565d08f34b912
Deivsonbf <deivsonb7@gmail.com>
2021-11-19 21:46:20 -0300

    novo server

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const app = require('./index');
 const port = process.env.PORT || 5000
 
 app.listen(port, (req, res) => {
-<<<<<<< HEAD
-	if( port == 5000){
+
+	if (port == 5000) {
 		console.log("Desenvolvimento")
 
-	}else{
+	} else {
 		console.log("produção")
 	}
+	
 })
-=======
-    console.log("Estou no ambiente de : " + process.env.NODE_ENV);
-})
->>>>>>> 87abe5de66ff105612fe3cf9809173722f1b1dd0
+
