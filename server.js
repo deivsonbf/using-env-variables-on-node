a4e7e160b9a01f80950754869210c249885a8876
Ubuntu <ubuntu@ip-172-31-6-236.sa-east-1.compute.internal>
2021-11-20 00:29:40 +0000

    criando env

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ const app = require('./index');
 const port = process.env.PORT || 5000
 
 app.listen(port, (req, res) => {
-    console.log(`Estou no ambiente de : ${process.env.NODE_ENV}`);
+	if( port == 5000){
+		console.log("Desenvolvimento")
+
+	}else{
+		console.log("produção")
+	}
 })
